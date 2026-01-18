ee0b56ffebd1c5581b37181cb560e2b08b28530c
vikashfernando <vikashdeshitha@gmail.com>
2026-01-18 23:13:23 +0530

    tst

--- 3 Java Stdin and Stdout II/src/Solution.java
+++ 3 Java Stdin and Stdout II/src/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
-        
+
 
 
         // Write your code here.
