cd97a54ad04a6fa6fa594a8b95f03d4b773877f4
vikashfernando <vikashdeshitha@gmail.com>
2026-01-20 12:19:18 +0530

    l11 f1 done

---
+++ 11 Java Strings Introduction/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+        //1 sum of lengths
+
+        System.out.println(A.length()+B.length());
+
+        //2
+
+
+
+        //3 capitalize
+
+    }
+}
+
+
+
