df50f95d5004b93a0c24e1631a7f1a0108795b89
vikashfernando <vikashdeshitha@gmail.com>
2026-01-20 12:30:25 +0530

    l11 done

--- 11 Java Strings Introduction/src/Solution.java
+++ 11 Java Strings Introduction/src/Solution.java
@@ -14,11 +14,19 @@ public class Solution {
         System.out.println(A.length()+B.length());
 
         //2
+        if(A.compareTo(B) > 0)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
 
+        //3 capitalize 1st letter
+        String capA;
+        String capB;
 
+        capA=A.substring(0,1).toUpperCase()+A.substring(1);
+        capB=B.substring(0,1).toUpperCase()+B.substring(1);
 
-        //3 capitalize
-
+        System.out.println(capA+" "+capB);
     }
 }
 
