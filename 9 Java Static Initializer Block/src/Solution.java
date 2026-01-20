5612bd405d175574ff7d7ae0021eb9a173c82add
vikashfernando <vikashdeshitha@gmail.com>
2026-01-20 06:41:41 +0530

    Update Solution.java

--- 9 Java Static Initializer Block/src/Solution.java
+++ 9 Java Static Initializer Block/src/Solution.java
@@ -1,5 +1,40 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    //Write your code here
+    static int H;
+    static int B;
+    static boolean flag;
+
+    static void getInput(){
+
+        Scanner i1=new Scanner(System.in);
+
+        System.out.println("height: ");
+        H=i1.nextInt();
+
+        System.out.println("breadth: ");
+        B=i1.nextInt();
+
+
+
     }
-}
+
+    
+
+
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
+
