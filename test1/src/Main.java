9ad116f3e7514676fa3cdc84338981f80acf360e
vikashfernando <vikashdeshitha@gmail.com>
2026-01-20 10:45:52 +0530

    update

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -2,7 +2,22 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+
+        //variables
+        int num1;
+        String txt1;
+
+        Scanner i1=new Scanner(System.in);
+
+        System.out.println("enter a number: ");
+        num1=i1.nextInt();
+
+        txt1=String.valueOf(num1);
+
+        System.out.println("int converted to String: "+txt1);
+
+        
+
 
 
 
